dacfada44404d112aeca92f4563a8ec16ae9a0fa
Shemaya Sandanayaka <shemaya.oshi26@gmail.com>
2023-06-04 15:35:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,4 +10,26 @@ def speak(text):
 	tts.save(filename)
 	playsound.playsound(filename)
 
+def get_audio():
+	r = sr.Recognizer()
+	with sr.Microphone() as source:
+		audio = r.listen(source)
+		input = ""
+
+		try:
+			# use Google API to recognize what we say
+			input = r.recognize_google(audio)
+			# prints the input given by the microphone
+			print(input)
+		except Exception as e:
+			# otherwise prints the exception
+			print("Exception" + str(e))
+
+	return input
+
 speak("Hello there")
+
+text = get_audio()
+
+if "hello" in text:
+	speak("hello, how are you?")
